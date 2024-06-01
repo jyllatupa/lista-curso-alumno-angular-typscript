eead816947048cca452a1a870182468f90b3e358
jyllatupa <chelsea_30_01@hotmail.com>
2024-05-31 23:36:42 -0500

    Proyecto final de lista curso-alumno en angular con Typescript

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,19 @@
+import { Alumno } from "src/app/models/alumno-to";
+
+export const alumnos: Alumno[] = [
+  {
+    nombre: 'Ricardo',
+    apellido: 'Perez',
+    promedio: 8.5
+  },
+  {
+    nombre: 'Maria',
+    apellido: 'Delgago',
+    promedio: 10
+  },
+  {
+    nombre: 'Jonathan',
+    apellido: 'Hidalgo',
+    promedio: 20
+  }
+]
---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,24 @@
+import { Curso } from "src/app/models/curso-to";
+
+export const cursos: Curso[] = [
+  {
+    materia: 'Javascript',
+    dificultad: 9,
+    requiere: 'HTML y CSS'
+  },
+  {
+    materia: 'Typescript',
+    dificultad: 10,
+    requiere: 'Javascript'
+  },
+  {
+    materia: 'Angular',
+    dificultad: 9,
+    requiere: 'HTML, CSS, JS y TS'
+  },
+  {
+    materia: 'React Js',
+    dificultad: 9,
+    requiere: 'HTML, JS y TS'
+  }
+]
